92c948e456c257511c6d020f604218b5d4b8c657
Psami-wondah <okechukwusamuel16@gmail.com>
2026-08-15 15:41:47 +0100

    feat: icon update

--- components/logo.tsx
+++ components/logo.tsx
@@ -9,7 +9,7 @@ export function Logo({ inverse = false }: { inverse?: boolean }) {
       aria-label="PH Otakus home"
     >
       <span
-        className="relative block h-10 w-[52px] overflow-hidden max-[820px]:h-[35px] max-[820px]:w-[46px]"
+        className="relative block h-12 w-[48px] overflow-hidden max-[820px]:h-[40px] max-[820px]:w-[40px]"
         aria-hidden="true"
       >
         <Image
@@ -20,13 +20,15 @@ export function Logo({ inverse = false }: { inverse?: boolean }) {
           }
           alt=""
           fill
-          sizes="52px"
+          sizes="48px"
           priority
           className="object-contain"
         />
       </span>
       <span className="flex flex-col leading-none">
-        <b className="font-display text-[1.28rem] tracking-[0.055em] max-[820px]:text-[1.04rem]">PH OTAKUS</b>
+        <b className="font-display text-[1.28rem] tracking-[0.055em] max-[820px]:text-[1.04rem]">
+          PH OTAKUS
+        </b>
       </span>
     </Link>
   );
